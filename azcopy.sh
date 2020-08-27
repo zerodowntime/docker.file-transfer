dfe06367700691d045e468e7f808b809f51b1193
Piotr Stawarski <piotr.stawarski@zerodowntime.pl>
2020-08-27 10:23:28 +0200

    use copy and add AZCOPY_OVERWRITE

--- azcopy.sh
+++ azcopy.sh
@@ -11,6 +11,10 @@ ensure-env AZURE_CLIENT_SECRET
 ensure-env AZURE_TENANT_ID
 ensure-env AZURE_CLIENT_ID
 
+# 'true', 'false', 'prompt', and 'ifSourceNewer'
+ensure-env AZCOPY_OVERWRITE "true"
+
+
 container_url="https://${STORAGE_ACCOUNT_NAME:?}.blob.core.windows.net/${CONTAINER_NAME:?}"
 container_path="${CONTAINER_PATH}"
 
@@ -23,5 +27,15 @@ AZCOPY_SPA_CLIENT_SECRET="${AZURE_CLIENT_SECRET:?}" azcopy login \
 # Silently try to create container.
 azcopy make "${container_url}" || :
 
-# Sync!
-azcopy sync "${UPLOAD_SOURCE}" "${container_url}/${container_path}"
+# Sync sucks, it cannot follow symlinks!
+
+# There's only two supported ways to use a wildcard character in a URL.
+# - You can use one just after the final forward slash (/) of a URL. This copies all of the files in a directory directly to the destination without placing them into a subdirectory.
+# - You can also use one in the name of a container as long as the URL refers only to a container and not to a blob. You can use this approach to obtain files from a subset of containers.
+
+azcopy copy \
+  --recursive \
+  --follow-symlinks \
+  --overwrite "$AZCOPY_OVERWRITE" \
+  "${UPLOAD_SOURCE}/*" \
+  "${container_url}/${container_path}"
